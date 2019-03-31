903a539cc1f521315cd7d9f7fa209471cf2ccf8b
EC2 Default User <ec2-user@ip-172-31-25-161.ap-southeast-1.compute.internal>
2019-03-31 14:38:58 +0000

    Add age

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_053128) do
+ActiveRecord::Schema.define(version: 2019_03_31_142934) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_053128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "age"
   end
 
 end
